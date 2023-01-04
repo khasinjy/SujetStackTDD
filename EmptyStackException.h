99316da4c8877ce27960490c770a2da3a90099d3
Khasinjy <k.praxele@gmail.com>
2023-01-04 11:52:42 +0100

    delete unnecessary includes

--- EmptyStackException.h
+++ EmptyStackException.h
@@ -1,9 +1,6 @@
 #ifndef __EMPTYSTACKEXCEPTION_H__
 #define __EMPTYSTACKEXCEPTION_H__
 
-#include <iostream>
-using namespace std;
-
 class EmptyStackException : public std::exception {
     public:
         const char* what(){
--- LackStackElementException.h
+++ LackStackElementException.h
@@ -1,9 +1,6 @@
 #ifndef __LACKSTACKELEMENTEXCEPTION_H__
 #define __LACKSTACKELEMENTEXCEPTION_H__
 
-#include <iostream>
-using namespace std;
-
 class LackStackElementException : public std::exception {
     public:
         const char* what(){
